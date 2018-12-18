83957045375468dad8661277433c3fb53df0ec6f
satoshi.ishikawa <satoshi.ishikawa@careritz.co.jp>
2018-12-18 12:20:11 +0900

    SSDテスト

---
+++ ssd_test.py
@@ -0,0 +1,105 @@
+import cv2
+import keras
+from keras.applications.imagenet_utils import preprocess_input
+from keras.backend.tensorflow_backend import set_session
+from keras.models import Model
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.misc import imread
+import tensorflow as tf
+
+from ssd import SSD300
+from ssd_utils import BBoxUtility
+import sys
+
+plt.rcParams['figure.figsize'] = (8, 8)
+plt.rcParams['image.interpolation'] = 'nearest'
+
+np.set_printoptions(suppress=True)
+
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.45
+set_session(tf.Session(config=config))
+
+
+voc_classes = ['face','hand']
+NUM_CLASSES = len(voc_classes) + 1
+
+
+input_shape=(300, 300, 3)
+model = SSD300(input_shape, num_classes=NUM_CLASSES)
+model.load_weights(args[1], by_name=True)
+bbox_util = BBoxUtility(NUM_CLASSES)
+
+
+import os
+input_data_path = 'data/test/'
+inputs = []
+images = []
+
+file_list = os.listdir(input_data_path)
+for file_name in file_list:
+    img_path = input_data_path + file_name
+    img = image.load_img(img_path, target_size=(300, 300))
+    img = image.img_to_array(img)
+    images.append(imread(img_path))
+    inputs.append(img.copy())
+
+inputs = preprocess_input(np.array(inputs))
+preds = model.predict(inputs, batch_size=1, verbose=1)
+results = bbox_util.detection_out(preds)
+
+
+a = model.predict(inputs, batch_size=1)
+b = bbox_util.detection_out(preds)
+
+
+for i, img in enumerate(images):
+    # Parse the outputs.
+    det_label = results[i][:, 0]
+    det_conf = results[i][:, 1]
+    det_xmin = results[i][:, 2]
+    det_ymin = results[i][:, 3]
+    det_xmax = results[i][:, 4]
+    det_ymax = results[i][:, 5]
+
+    # Get detections with confidence higher than 0.6.
+    top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.6]
+
+    top_conf = det_conf[top_indices]
+    top_label_indices = det_label[top_indices].tolist()
+    top_xmin = det_xmin[top_indices]
+    top_ymin = det_ymin[top_indices]
+    top_xmax = det_xmax[top_indices]
+    top_ymax = det_ymax[top_indices]
+
+    colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
+
+    plt.imshow(img / 255.)
+    currentAxis = plt.gca()
+
+    for i in range(top_conf.shape[0]):
+        
+        if  top_conf[i] < 0.18:
+            continue 
+        xmin = int(round(top_xmin[i] * img.shape[1]))
+        ymin = int(round(top_ymin[i] * img.shape[0]))
+        xmax = int(round(top_xmax[i] * img.shape[1]))
+        ymax = int(round(top_ymax[i] * img.shape[0]))
+        
+        print ('X:' + str(top_xmin[i]) + ':' + str(top_xmax[i]) + 'Y:' + str(top_ymin[i]) + ':' + str(top_ymax[i]))
+        
+        #print('X:' + str(xmax) + ':' + str(xmin) + 'Y:' + str(ymax) + ':' + str(ymin))
+        
+        score = top_conf[i]
+        label = int(top_label_indices[i])
+        label_name = voc_classes[label - 1]
+        display_txt = '{:0.2f}, {}'.format(score, label_name)
+        coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
+        color = colors[label]
+        currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
+        currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
+    
+    plt.show()
+    
